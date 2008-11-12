077af600e1c5f1c852f201988be5a7f3455c1826
Peter Solymos <solymos@ualberta.ca>
2008-11-12 00:00:00 +0000

    version 2.1-0

--- R/mefaCheck.R
+++ R/mefaCheck.R
@@ -8,6 +8,7 @@ function(x)
     re <- list()
 
 # when extracting or aggregating, it can be shorter, when e.g. x$samp <- NULL
+# so leave it as TRUE
 #    mt[1] <- length(x) == 5
     mt[2] <- is.matrix(x$xtab)
     if (!is.null(x$segm)) {
@@ -29,27 +30,28 @@ function(x)
         mt[9] <- is.data.frame(x$taxa)
         mt[10] <- identical(colnames(x$xtab), rownames(x$taxa))
         }
+# re list contains the final result and description of problems
     re[[1]] <- all(mt == TRUE)
     i <- 2
     if (!mt[1]) {re[[i]] <- "object length is not 5"
         i <- i + 1}
-    if (!mt[2]) {re[[(i)]] <- "'$xtab' is not matrix"
+    if (!mt[2]) {re[[i]] <- "'$xtab' is not matrix"
         i <- i + 1}
-    if (!mt[3]) {re[[(i)]] <- "'$segm' is not list"
+    if (!mt[3]) {re[[i]] <- "'$segm' is not list"
         i <- i + 1}
-    if (!mt[4]) {re[[(i)]] <- "sum of '$xtab' and sums in '$segm' are not equal"
+    if (!mt[4]) {re[[i]] <- "sum of '$xtab' and sums in '$segm' are not equal"
         i <- i + 1}
-    if (!mt[5]) {re[[(i)]] <- "rownames in '$xtab' and '$segm' are not identical"
+    if (!mt[5]) {re[[i]] <- "rownames in '$xtab' and '$segm' are not identical"
         i <- i + 1}
-    if (!mt[6]) {re[[(i)]] <- "colnames in '$xtab' and '$segm' are not identical"
+    if (!mt[6]) {re[[i]] <- "colnames in '$xtab' and '$segm' are not identical"
         i <- i + 1}
-    if (!mt[7]) {re[[(i)]] <- "'$samp' is not 'data.frame'"
+    if (!mt[7]) {re[[i]] <- "'$samp' is not 'data.frame'"
         i <- i + 1}
-    if (!mt[8]) {re[[(i)]] <- "rownames in '$xtab' and '$samp' are not identical"
+    if (!mt[8]) {re[[i]] <- "rownames in '$xtab' and '$samp' are not identical"
         i <- i + 1}
-    if (!mt[9]) {re[[(i)]] <- "'$taxa' is not 'data.frame'"
+    if (!mt[9]) {re[[i]] <- "'$taxa' is not 'data.frame'"
         i <- i + 1}
-    if (!mt[10]) re[[(i)]] <- "colnames in '$xtab' and rownames in '$taxa' are not identical"
+    if (!mt[10]) re[[i]] <- "colnames in '$xtab' and rownames in '$taxa' are not identical"
 
     return(re)
 }
--- R/summary.mefa.R
+++ R/summary.mefa.R
@@ -1,12 +1,12 @@
 `summary.mefa` <-
-function(object, ...)
+function(object, nlist = 10, ...)
 {
 x <- object
 out <- list(
-    srich = rowSums(x$xtab > 0),
-    ninds = rowSums(x$xtab),
-    occup = colSums(x$xtab > 0),
-    abund = colSums(x$xtab),
+    s.rich = rowSums(x$xtab > 0),
+    s.abu = rowSums(x$xtab),
+    t.occ = colSums(x$xtab > 0),
+    t.abu = colSums(x$xtab),
     ntot = sum(x$xtab),
     mfill = sum(x$xtab > 0)/(dim(x)[1]*dim(x)[2]),
     nsamp = dim(x)[1],
@@ -18,5 +18,6 @@ out <- list(
     drop.zero = attr(x, "drop.zero"),
     xtab.fixed = attr(x, "xtab.fixed"))
 class(out) <- c("summary.mefa")
+attr(out, "nlist") <- nlist
 return(out)}
 
